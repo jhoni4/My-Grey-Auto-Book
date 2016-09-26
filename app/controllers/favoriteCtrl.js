eca600c498018630e944eaf8fe3cdd474258663b
yonatan molla <jhoni42002@yahoo.com>
2016-09-26 10:16:43 -0500

    looping added to favorites

--- app/controllers/favoriteCtrl.js
+++ app/controllers/favoriteCtrl.js
@@ -10,26 +10,13 @@ app.controller("FavoriteCtrl", function($scope, SearchFactory)  {
   SearchFactory.getFavoriteFromFb()
   .then( (favObj) => {
     $scope.favorites = favObj;
-    $scope.favorites.ratings = favObj.ratings;
-    $scope.favoriteSource.data[0].label = favObj[0].year + favObj[0].make + favObj[0].model;
-    $scope.favoriteSource.data[1].label = favObj[1].year + favObj[1].make + favObj[1].model;
-    $scope.favoriteSource.data[2].label = favObj[2].year + favObj[2].make + favObj[2].model;
-    $scope.favoriteSource.data[3].label = favObj[3].year + favObj[3].make + favObj[3].model;
-    $scope.favoriteSource.data[4].label = favObj[4].year + favObj[4].make + favObj[4].model;
-    $scope.favoriteSource.data[5].label = favObj[5].year + favObj[5].make + favObj[5].model;
-    $scope.favoriteSource.data[6].label = favObj[6].year + favObj[6].make + favObj[6].model;
-    $scope.favoriteSource.data[7].label = favObj[7].year + favObj[7].make + favObj[7].model;
-    $scope.favoriteSource.data[8].label = favObj[8].year + favObj[8].make + favObj[8].model;
-    $scope.favoriteSource.data[0].value = $scope.calculateAverage(favObj[0].ratings);
-    $scope.favoriteSource.data[1].value = $scope.calculateAverage(favObj[1].ratings);
-    $scope.favoriteSource.data[2].value = $scope.calculateAverage(favObj[2].ratings);
-    $scope.favoriteSource.data[3].value = $scope.calculateAverage(favObj[3].ratings);
-    $scope.favoriteSource.data[4].value = $scope.calculateAverage(favObj[4].ratings);
-    $scope.favoriteSource.data[5].value = $scope.calculateAverage(favObj[5].ratings);
-    $scope.favoriteSource.data[6].value = $scope.calculateAverage(favObj[6].ratings);
-    $scope.favoriteSource.data[7].value = $scope.calculateAverage(favObj[7].ratings);
-    $scope.favoriteSource.data[8].value = $scope.calculateAverage(favObj[8].ratings);
+    for (var i = 0; i < favObj.length; i++) {
+      $scope.favoriteSource.data.push({label: "", value: ""});
+      $scope.favoriteSource.data[i].label = favObj[i].year + favObj[i].make + favObj[i].model;
+      $scope.favoriteSource.data[i].value = $scope.calculateAverage(favObj[i].ratings);
+    };
   });
+
 
 $scope.calculateAverage = function(MyData){
   // console.log("MyData", MyData);
@@ -89,48 +76,14 @@ $scope.calculateAverage = function(MyData){
   };
 
 //chart favorite///////////////json
+//chart favorite///////////////json
 
 $scope.favoriteSource = {
         chart: {
             caption: "These are your favorites picks",
             subCaption: "with their overall average rating",
         },
-        data:[{
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        },
-        {
-            label: "",
-            value: ""
-        }]
+        data:[]
       };
 
 
